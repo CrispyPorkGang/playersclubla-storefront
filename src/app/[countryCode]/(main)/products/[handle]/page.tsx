7ac4a5d9afd8366365b2d80e9cd16ff0a36327ac
unknown <lucas@lucasimportsinc.com>
2024-11-01 11:20:04 -0700

    updated env and counrtry code/page

--- src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -25,10 +25,10 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   }
 
   return {
-    title: `${product.title} | Medusa Store`,
+    title: `${product.title} | Players Club LA`,
     description: `${product.title}`,
     openGraph: {
-      title: `${product.title} | Medusa Store`,
+      title: `${product.title} | Players Club LA`,
       description: `${product.title}`,
       images: product.thumbnail ? [product.thumbnail] : [],
     },
