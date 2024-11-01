5c78682bfe009d25df276be0af2dc6f5851439c1
unknown <lucas@lucasimportsinc.com>
2024-11-01 11:05:41 -0700

    updated params on collection/page

--- src/app/[countryCode]/(main)/collections/[handle]/page.tsx
+++ src/app/[countryCode]/(main)/collections/[handle]/page.tsx
@@ -5,8 +5,7 @@ import {
   getCollectionByHandle,
   getCollectionsList,
 } from "@lib/data/collections"
-import { listRegions } from "@lib/data/regions"
-import { StoreCollection, StoreRegion } from "@medusajs/types"
+import { StoreCollection } from "@medusajs/types"
 import CollectionTemplate from "@modules/collections/templates"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 
@@ -20,64 +19,31 @@ type Props = {
 
 const PRODUCT_LIMIT = 12
 
-export async function generateStaticParams() {
-  const { collections } = await getCollectionsList()
-
-  if (!collections) {
-    return []
-  }
-
-  const countryCodes = await listRegions().then(
-    (regions: StoreRegion[]) =>
-      regions
-        ?.map((r) => r.countries?.map((c) => c.iso_2))
-        .flat()
-        .filter(Boolean) as string[]
-  )
-
-  const collectionHandles = collections.map(
-    (collection: StoreCollection) => collection.handle
-  )
-
-  const staticParams = countryCodes
-    ?.map((countryCode: string) =>
-      collectionHandles.map((handle: string | undefined) => ({
-        countryCode,
-        handle,
-      }))
-    )
-    .flat()
-
-  return staticParams
-}
-
-export async function generateMetadata(props: Props): Promise<Metadata> {
-  const params = await props.params;
-  const collection = await getCollectionByHandle(params.handle)
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const resolvedParams = await params;
+  const collection = await getCollectionByHandle(resolvedParams.handle);
 
   if (!collection) {
-    notFound()
+    notFound();
   }
 
-  const metadata = {
+  return {
     title: `${collection.title} | Medusa Store`,
     description: `${collection.title} collection`,
-  } as Metadata
-
-  return metadata
+  };
 }
 
 export default async function CollectionPage(props: Props) {
-  const searchParams = await props.searchParams;
-  const params = await props.params;
-  const { sortBy, page } = searchParams
-
-  const collection = await getCollectionByHandle(params.handle).then(
-    (collection: StoreCollection) => collection
-  )
+  const [params, searchParams] = await Promise.all([
+    props.params,
+    props.searchParams
+  ]);
+  
+  const { sortBy, page } = searchParams;
+  const collection = await getCollectionByHandle(params.handle);
 
   if (!collection) {
-    notFound()
+    notFound();
   }
 
   return (
@@ -87,5 +53,5 @@ export default async function CollectionPage(props: Props) {
       sortBy={sortBy}
       countryCode={params.countryCode}
     />
-  )
+  );
 }
